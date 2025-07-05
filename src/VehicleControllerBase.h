fac25e5320a94741ee89036ac2dab6f291ed0658
Martin Budden <mjbudden@gmail.com>
2025-07-05 07:21:47 +0100

    Removed failsafe from VehicleControllerBase.

--- src/VehicleControllerBase.h
+++ src/VehicleControllerBase.h
@@ -18,15 +18,6 @@ public:
         uint8_t kf;
     };
     enum { TYPE_NOT_SET= 0, SELF_BALANCING_ROBOT = 1, AIRCRAFT = 2 };
-    enum failsafe_phase_e {
-        FAILSAFE_IDLE = 0,
-        FAILSAFE_RX_LOSS_DETECTED,
-        FAILSAFE_LANDING,
-        FAILSAFE_LANDED,
-        FAILSAFE_RX_LOSS_MONITORING,
-        FAILSAFE_RX_LOSS_RECOVERED,
-        FAILSAFE_GPS_RESCUE
-    };
 public:
     VehicleControllerBase(uint32_t type, uint32_t PID_Count) : _type(type),  _PID_Count(PID_Count) {}
 public:
@@ -37,9 +28,7 @@ public:
     inline float getPitchAngleDegreesRaw() const { return _pitchAngleDegreesRaw; }
     inline float getRollAngleDegreesRaw() const { return _rollAngleDegreesRaw; }
     inline float getYawAngleDegreesRaw() const { return _yawAngleDegreesRaw; }
-    inline failsafe_phase_e getFailsafePhase() const { return _failsafePhase; }
     
-
     virtual void loop(float deltaT, uint32_t tickCount) = 0;
     virtual void updateOutputsUsingPIDs(const xyz_t& gyroRPS, const xyz_t& acc, const Quaternion& orientation, float deltaT) = 0;
     virtual uint32_t getOutputPowerTimeMicroSeconds() const = 0; //<! time taken to write output power to the motors, for instrumentation
@@ -52,7 +41,6 @@ protected:
     const uint32_t _type;
     const uint32_t _PID_Count;
     const TaskBase* _task {nullptr};
-    failsafe_phase_e _failsafePhase {FAILSAFE_IDLE};
     float _pitchAngleDegreesRaw {0.0F};
     float _rollAngleDegreesRaw {0.0F};
     float _yawAngleDegreesRaw {0.0F};
