59ca7a4605a006ddf7cafc4c55ca32248db9820f
Martin Budden <mjbudden@gmail.com>
2026-04-18 07:13:46 +0100

    Standardized order of acc and gyro in ImuFiltersBase.

--- src/imu_filters_base.h
+++ src/imu_filters_base.h
@@ -6,5 +6,5 @@ struct xyz_t;
 class ImuFiltersBase {
 public:
     virtual ~ImuFiltersBase() = default;
-    virtual void filter(xyz_t& gyro_rps, xyz_t& acc, float delta_t, Debug& debug) { (void)gyro_rps; (void)acc; (void)delta_t; (void)debug; }
+    virtual void filter(xyz_t& acc, xyz_t& gyro_rps, float delta_t, Debug& debug) { (void)gyro_rps; (void)acc; (void)delta_t; (void)debug; }
 };
